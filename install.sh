19171a4966e7a48fb0c9a235771e9478d2014340
LDLCOL <regie@ldlc-ol.com>
2023-02-07 11:40:16 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 apt clean
 apt autoremove -y
 apt update -y
@@ -7,3 +9,6 @@ apt install --no-install-recommends xdotool unclutter sed
 apt-get install --no-install-recommends chromium-browser
 
 echo '[[ -z $DISPLAY && $XDG_VTNR -eq 1 ]] && startx' >> /home/pi/.bash_profile
+
+apt clean
+apt autoremove -y
