1f9d357a4148f145919c0e2636134f6d7f6790c2
LDLCOL <regie@ldlc-ol.com>
2023-02-07 11:51:02 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,9 +4,7 @@ apt clean
 apt autoremove -y
 apt update -y
 apt upgrade -y
-apt-get install --no-install-recommends xserver-xorg x11-xserver-utils xinit openbox
-apt install --no-install-recommends xdotool unclutter sed
-apt-get install --no-install-recommends chromium-browser
+apt install --no-install-recommends xserver-xorg x11-xserver-utils xinit openbox xdotool unclutter sed chromium-browser -y
 
 echo '[[ -z $DISPLAY && $XDG_VTNR -eq 1 ]] && startx' >> /home/pi/.bash_profile
 
