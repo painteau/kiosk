c381e7d218d27c7316886c554a0877e2ae5dbbc1
LDLCOL <regie@ldlc-ol.com>
2023-02-07 11:39:22 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,9 @@
+apt clean
+apt autoremove -y
+apt update -y
+apt upgrade -y
+apt-get install --no-install-recommends xserver-xorg x11-xserver-utils xinit openbox
+apt install --no-install-recommends xdotool unclutter sed
+apt-get install --no-install-recommends chromium-browser
+
+echo '[[ -z $DISPLAY && $XDG_VTNR -eq 1 ]] && startx' >> /home/pi/.bash_profile
