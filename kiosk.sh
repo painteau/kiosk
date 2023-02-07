dbaba26c4c891318b87a0b7838634feee54f6668
LDLCOL <regie@ldlc-ol.com>
2023-02-07 11:42:22 +0100

    Update kiosk.sh

--- kiosk.sh
+++ kiosk.sh
@@ -11,7 +11,7 @@ unclutter -idle 0.5 -root &
 sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/$USER/.config/chromium/Default/Preferences
 sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/$USER/.config/chromium/Default/Preferences
 
-chromium-browser --noerrdialogs --disable-infobars --kiosk https://pimylifeup.com https://www.adafruit.com &
+chromium-browser --start-fullscreen --kiosk --incognito --noerrdialogs --disable-translate --no-first-run --fast --fast-start --disable-infobars --disable-features=TranslateUI --disk-cache-dir=/dev/null  --password-store=basic --disable-pinch --overscroll-history-navigation=disabled --disable-features=TouchpadOverscrollHistoryNavigation https://www.meteofrance.com/ &
 
 # while true; do
 #   xdotool keydown ctrl+Next; xdotool keyup ctrl+Next;
