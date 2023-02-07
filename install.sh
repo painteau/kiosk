a25a460f1e94674ecebd159318acce79c66e895e
LDLCOL <regie@ldlc-ol.com>
2023-02-07 11:51:25 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,10 +5,9 @@ apt autoremove -y
 apt update -y
 apt upgrade -y
 apt install --no-install-recommends xserver-xorg x11-xserver-utils xinit openbox xdotool unclutter sed chromium-browser -y
-
-echo '[[ -z $DISPLAY && $XDG_VTNR -eq 1 ]] && startx' >> /home/pi/.bash_profile
-
 apt clean
 apt autoremove -y
+
+echo '[[ -z $DISPLAY && $XDG_VTNR -eq 1 ]] && startx' >> /home/pi/.bash_profile
 
 cat kiosk.sh >> /etc/xdg/openbox/autostart 
