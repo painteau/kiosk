e9dd98a47b4803d5ef212ef7aabaefb71e15596b
LDLCOL <regie@ldlc-ol.com>
2023-02-07 12:53:46 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 # Update and install and clean
 apt update -y
 apt upgrade -y
-apt install --no-install-recommends xserver-xorg x11-xserver-utils xinit openbox xdotool unclutter sed chromium-browser -y
+apt install --no-install-recommends xserver-xorg x11-xserver-utils xinit openbox xdotool unclutter sed chromium-browser midori -y
 apt purge wolfram-engine scratch nuscratch sonic-pi idle3 smartsim java-common libreoffice* -y
 apt clean
 apt autoremove -y
