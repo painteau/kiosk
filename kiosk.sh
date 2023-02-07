0cd7598e4d10c3451f2529f8840658d0d2446a7b
LDLCOL <regie@ldlc-ol.com>
2023-02-07 11:35:42 +0100

    kiosk.sh

---
+++ kiosk.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+xset s noblank
+xset s off
+xset -dpms
+unclutter -idle 0.5 -root &
+
+sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/$USER/.config/chromium/Default/Preferences
+sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/$USER/.config/chromium/Default/Preferences
+
+/usr/bin/chromium-browser --noerrdialogs --disable-infobars --kiosk https://pimylifeup.com https://www.adafruit.com &
+
+# while true; do
+#   xdotool keydown ctrl+Next; xdotool keyup ctrl+Next;
+#   sleep 10
+# done
